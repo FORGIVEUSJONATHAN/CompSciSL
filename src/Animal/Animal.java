7c864cbf157a42b8eafad7cb37179ade80a4a813
JonathanXu <Xzj405805>
2019-03-11 09:43:38 -0600

    Static

---
+++ src/Animal/Animal.java
@@ -0,0 +1,12 @@
+package Animal;
+
+public class Animal {
+
+    public int age = 0;
+    public static int count = 0;
+
+    public Animal() {
+        age++;
+        count++;
+    }
+}
---
+++ src/Animal/AnimalMain.java
@@ -0,0 +1,15 @@
+package Animal;
+
+public class AnimalMain {
+    public static void main(String arg[]){
+
+        Animal A1 = new Animal();
+        System.out.println(A1.age);
+        System.out.println(A1.count);
+        Animal A2 = new Animal();
+        System.out.println(A2.age);
+        System.out.println(A2.count);
+
+    }
+
+}
